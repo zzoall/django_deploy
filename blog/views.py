b5f5d7c0ef4206c4437ec14aeebfd45fa9e8ff0c
YeonjiKim0316 <kimyeonji3@gmail.com>
2023-02-15 16:04:52 +0900

    0215 오후 실습

--- blog/views.py
+++ blog/views.py
@@ -11,18 +11,29 @@ class PostList(ListView):  # post_list 라고 생긴 template과 model을 조합
     # post_list에 전달할 전체 객체명을 바꿔줍니다.
     context_object_name = 'posts'
 
-    # ListView를 상속받으면 전달받은 get_context_data라는 함수를 오버라이딩합니다
-    def get_context_data(self, **kwargs): # 함수의 파라미터를 개수 안정해서 k-v 순으로 만들어 보내겠다
-        # 속성명 = 값  키:밸류
+    # ListView를 상속받으면서 전달받은 get_context_data라는 함수를 오버라이딩합니다
+    def get_context_data(self, **kwargs): # 함수의 파라미터를 개수 안 정해서 k-v 순으로 만들어 보내겠다
+        # 속성명 = 값 -> 파이썬의 namespace에서는 키:밸류 순으로 관리됩니다
         context = super(PostList, self).get_context_data(**kwargs)
         context['first_post'] = Post.objects.all().last() # first_post라는 이름으로 하나 더 값을 만들어서 전달할게요
         # 필요한 값들을 ORM으로 뽑아서 가져올 수 있습니다.
         return context
 
     
+# blog 폴더(앱)에서 post_detail.html로 가고 있는 객체를 posts라고 이름을 변경해보세요
+# 해당 변수명으로 받은 객체들에 더해 subject 라는 이름으로 제목 데이터만 전달하는 추가 변수를 달아주세요
 
 class PostDetail(DetailView):  # post_detail 라고 생긴 template과 model을 조합합니다. 
     model = Post
+    # context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs): # 함수의 파라미터를 개수 안 정해서 k-v 순으로 만들어 보내겠다
+        # 속성명 = 값 -> 파이썬의 namespace에서는 키:밸류 순으로 관리됩니다
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        context['subject'] = Post.objects.get(pk).title # first_post라는 이름으로 하나 더 값을 만들어서 전달할게요
+        # 필요한 값들을 ORM으로 뽑아서 가져올 수 있습니다.
+        return context
+
 
 # Create your views here.
 # V (view) - 화면에 출력되는 부분을 책임집니다. 
