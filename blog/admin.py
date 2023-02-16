8baf579e04d4f3005bc01e3399c92ab02110e4e8
YeonjiKim0316 <kimyeonji3@gmail.com>
2023-02-16 15:59:25 +0900

    with add tag in post_detail

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category  # 현재 경로의 models.py 안에 있는 Post를 가져다 쓰겠음 
+from .models import Post, Category, Tag  # 현재 경로의 models.py 안에 있는 Post를 가져다 쓰겠음 
 
 # Register your models here.
 admin.site.register(Post)
@@ -14,3 +14,9 @@ admin.site.register(Category, CategoryAdmin)
 
 # 최종적으로 admin에서 Tags 항목을 우리가 손볼 수 있어야 할 거고요 
 # 그래서 나온 태그를 post_detail.html에서 출력되도록 만들어주세요
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('tagName', )}
+
+
+admin.site.register(Tag, TagAdmin)
