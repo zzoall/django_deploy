5a67b09acea1fd4e1dca6275ccd6d63dc80fe718
YeonjiKim0316 <kimyeonji3@gmail.com>
2023-02-17 11:52:49 +0900

    categories 완성 필요

--- blog/urls.py
+++ blog/urls.py
@@ -9,13 +9,14 @@ app_name = "blog"
 
 # blog 앱 내부 경로를 지정하는 부분
 urlpatterns = [
+    path('', views.BlogHome.as_view(paginate_by=5), name="home"),
     # paginate_by=개수
-    path('', views.BlogList.as_view(paginate_by=5), name="home"), # '' : blog 뒤에 달린 주소가 없음을 의미함 
+    path('post_list/', views.PostList.as_view(), name="post_list"), # '' : blog 뒤에 달린 주소가 없음을 의미함 
     path('<int:pk>/', views.PostDetail.as_view()),
     path('about/', views.about_me, name="about_me"), # name="별명" // Alias
     path('contact/', views.contact, name="contact"),  
     # "blog" 라는 app의 "contact" 라는 별명으로 주소/blog/contact/를 호출할 수 있게 됩니다
-    path('post_list/', views.PostList.as_view(), name="post_list"),  # paginate_by=10 여기에 줄 수도 있습니다
+
     # path('index2/', views.index2)  # 주소/blog/index2
 ]
 
